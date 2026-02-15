0a50a65794154ced5f139a5ffba481046166e21e
Keshav Kinger <keshavkinger@Keshavs-MacBook-Air-2.local>
2026-02-15 20:29:47 +0530

    Fix Admin model import path

--- routes/adminAuthRoutes.js
+++ routes/adminAuthRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const Admin = require("../models/Admin");
+const Admin = require("../models/admin");
 
 // LOGIN
 router.post("/login", async (req, res) => {
