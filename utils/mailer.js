f1bb6c4e6b94ccda9f0d4cce374af714ff8dda96
Keshav Kinger <keshavkinger@Keshavs-MacBook-Air-2.local>
2026-05-23 19:38:40 +0530

    Force SMTP IPv4 connections

--- utils/mailer.js
+++ utils/mailer.js
@@ -21,6 +21,7 @@ function getSmtpTransporter() {
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT || 587),
       secure: process.env.SMTP_SECURE === "true",
+      family: 4,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS?.replace(/\s/g, ""),
